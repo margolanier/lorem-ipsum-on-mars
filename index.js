9957410f757dc086eb304fb392e0cb33f8a68431
Caitlin Daitch <caitlin.daitch@gmail.com>
2017-01-06 19:18:02 -0500

    Replace spaces with tabs

--- index.js
+++ index.js
@@ -8,26 +8,26 @@ var Lyrics = mongoose.model("Lyrics");
 app.set("view engine", "hbs");
 
 app.engine(".hbs", hbs({
-  extname: ".hbs",
-  partialsDir: "views/",
-  layoutsDir: "views/",
-  defaultLayout: "layout-main"
+	extname: ".hbs",
+	partialsDir: "views/",
+	layoutsDir: "views/",
+	defaultLayout: "layout-main"
 }));
 
 app.use("/assets", express.static("public"))
 
 app.get("/api/lyrics", function(req, res){
-  Lyrics.find().then(function(lyrics){
-    res.json(lyrics);
-  });
+	Lyrics.find().then(function(lyrics){
+		res.json(lyrics);
+	});
 });
 
 app.get("/*", function(req, res){
-  res.render("app-welcome");
+	res.render("app-welcome");
 });
 
 app.set("port", process.env.PORT || 3001);
 
 app.listen(app.get("port"), function(){
-  console.log("It's aliiive!");
+	console.log("It's aliiive!");
 });
--- public/js/app.js
+++ public/js/app.js
@@ -2,79 +2,79 @@
 
 (function(){
 
-  angular
-  .module("lyrics", [
-    "ui.router",
-    "ngResource"
-  ])
-  .config([
-    "$stateProvider",
-    "$locationProvider",
-    Router
-  ])
-  .factory("LyricsFactory", [
-    "$resource",
-    LyricsFactory
-  ])
-  .controller("LyricsController", [
-    "LyricsFactory",
-    LyricsController
-  ])
+	angular
+	.module("lyrics", [
+		"ui.router",
+		"ngResource"
+	])
+	.config([
+		"$stateProvider",
+		"$locationProvider",
+		Router
+	])
+	.factory("LyricsFactory", [
+		"$resource",
+		LyricsFactory
+	])
+	.controller("LyricsController", [
+		"LyricsFactory",
+		LyricsController
+	])
 
-  function Router($stateProvider, $locationProvider){
-    $locationProvider.html5Mode(true);
-    $stateProvider
-    .state("welcome", {
-      url: "/",
-      templateUrl: "/assets/html/lyrics-welcome.html"
-    })
-    .state("lyrics", {
-      url: "/lyrics",
-      templateUrl: "/assets/html/lyrics-generator.html",
-      controller: "LyricsController",
-      controllerAs: "LyricsViewModel"
-    })
-  };
+	function Router($stateProvider, $locationProvider){
+		$locationProvider.html5Mode(true);
+		$stateProvider
+		.state("welcome", {
+			url: "/",
+			templateUrl: "/assets/html/lyrics-welcome.html"
+		})
+		.state("lyrics", {
+			url: "/lyrics",
+			templateUrl: "/assets/html/lyrics-generator.html",
+			controller: "LyricsController",
+			controllerAs: "LyricsViewModel"
+		})
+	};
 
-  function LyricsFactory($resource){
-    var Lyrics = $resource("/api/lyrics", {}, {
-      update: {method: "PUT"}
-    });
+	function LyricsFactory($resource){
+		var Lyrics = $resource("/api/lyrics", {}, {
+			update: {method: "PUT"}
+		});
 
-    Lyrics.find = function(property, value, callback){
-      Lyrics.all.$promise.then(function(){
-        Lyrics.all.forEach(function(lyrics){
-          if(lyrics[property] == value) callback(lyrics);
-        });
-      });
-    };
+		Lyrics.find = function(property, value, callback){
+			Lyrics.all.$promise.then(function(){
+				Lyrics.all.forEach(function(lyrics){
+					if(lyrics[property] == value) callback(lyrics);
+				});
+			});
+		};
 
-    Lyrics.all = Lyrics.query();
+		Lyrics.all = Lyrics.query();
 
-    return Lyrics;
-  };
+		return Lyrics;
+	};
 
-  function LyricsController(lyrics){
-    var vm = this;
-    vm.lyrics = lyrics.all;
+	function LyricsController(lyrics){
+		var vm = this;
+		vm.lyrics = lyrics.all;
 
-    vm.num1 = randomNumber();
-    vm.num2 = randomNumber();
-    vm.num3 = randomNumber();
-    vm.num4 = randomNumber();
-    vm.num5 = randomNumber();
-    vm.num6 = randomNumber();
-    vm.num7 = randomNumber();
-    vm.num8 = randomNumber();
-    vm.num9 = randomNumber();
-    vm.num10 = randomNumber();
-  };
+		vm.num1 = randomNumber();
+		vm.num2 = randomNumber();
+		vm.num3 = randomNumber();
+		vm.num4 = randomNumber();
+		vm.num5 = randomNumber();
+		vm.num6 = randomNumber();
+		vm.num7 = randomNumber();
+		vm.num8 = randomNumber();
+		vm.num9 = randomNumber();
+		vm.num10 = randomNumber();
+	};
 
-  function randomNumber(){
-    var randomDecimal = Math.random() * 55;
-    var num = Math.round(randomDecimal);
+	function randomNumber(){
+		var randomDecimal = Math.random() * 55;
+		var num = Math.round(randomDecimal);
 
-    return num
-  };
+		return num
+	};
 
 })();
